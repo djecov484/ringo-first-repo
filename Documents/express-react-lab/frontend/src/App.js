dca546b5ea50414dbaaf07ebcd3abaf2cd0e038b
djecov484 <djecov484@gmail.com>
2021-10-30 11:00:33 -0700

    frontend complete

---
+++ Documents/express-react-lab/frontend/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {Route, Switch} from "react-router-dom"
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+
+
+
+function App() {
+
+  const URL ="https://hidden-earth-90438.herokuapp.com/"
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/projects">
+        <Projects URL={URL} />
+      </Route>
+      <Route path="/about">
+        <About URL={URL} />
+      </Route>
+      </Switch>
+      <Footer />
+
+    </div>
+);
+}
+
+export default App;
+
+
+
+
---
+++ Documents/express-react-lab/frontend/src/components/Header.js
@@ -0,0 +1,38 @@
+import {Link} from "react-router-dom"
+
+
+
+
+function Header (props) {
+    const navStyle = {
+        display: "flex",
+        justifyContent: "space-around",
+        border: "3px solidblack",
+        padding: "8px",
+        width:"90%",
+        margin: "auto",
+    }
+    return (
+       <header>
+          <h1>My Portfolio Page</h1>
+           <nav style ={navStyle}>
+               <Link to="/">
+                   <div>Home</div>
+               </Link>
+               <Link to="/about">
+                   <div>About</div>
+               </Link>
+               <Link to="/projects">
+                   <div>PROJECTS</div>
+               </Link>
+           </nav>
+
+       </header>
+
+
+
+    )
+
+
+}
+export default Header
